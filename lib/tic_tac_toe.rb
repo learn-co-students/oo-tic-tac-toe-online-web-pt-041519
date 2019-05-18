da3d9ccd087d9518036c2fddc276ecb93ced9ccc
Conor Owens <conorj.owens@gmail.com>
2019-05-18 01:38:45 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,60 @@
+class TicTacToe
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+ 
+ WIN_COMBINATIONS = [ 
+   [0,1,2], 
+   [3,4,5], 
+   [6,7,8],
+   [0,3,6], 
+   [1,4,7], 
+   [2,5,8], 
+   [0,4,8], 
+   [6,4,2]
+   ]
+  
+  def input_to_index(string)
+    @string = string
+    @string.to_i - 1 
+  end 
+  
+  def move(number, token)
+    @board[number] = token
+  end
+  
+  def position_taken?(index)
+    index.between?(0,8) && @board[index] != " "
+  end
+  
+  def valid_move?(index)
+    index.between?(0,8) && @board[index] == " "
+  end
+  
+  def turn
+    move = gets.chomp
+    index = input_to_index(move)
+    unless valid_move?(input_to_index(index))
+      puts "Try again!"
+      index = input_to_index(gets.chomp)
+    end
+    @board[index] = current_player
+    display_board
+  end
+  
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+ 
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+ 
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+end

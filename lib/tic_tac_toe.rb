e02a5a1c8cfb187dc447a1e5fdc80c31b925b240
Cody Frank <cody.frank30@gmail.com>
2019-05-19 02:02:11 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,59 @@
+class TicTacToe
+  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+
+  def initialize
+    @board = Array.new(9, " ")
+  end
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(index, token = "X")
+    @board[index] = token
+  end
+
+  def position_taken?(i)
+    value = false
+    if @board[i] == "X"
+      value = true
+    elsif @board[i] == "O"
+      value = true
+    end
+    value
+  end
+
+  def valid_move?(i)
+    valid = true
+    if i > 9 || i < 0
+      valid = false
+    elsif @board[i] == "X"
+      valid = false
+    elsif @board[i] == "O"
+      valid = false
+    end
+    valid
+  end
+
+  def turn_count
+    count = 0
+    @board.each do |index|
+      if index == "X"
+        count += 1
+      elsif index == "O"
+        count += 1
+      end
+    end
+    count
+  end
+
+
+end
